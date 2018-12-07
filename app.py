29d085b035209a82b40f81547adf32fa9b22199f
Marcus Wilson <mswilson4040@hotmail.com>
2018-12-06 19:34:55 -0800

    added signup form

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+from models import db, User
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
+app.secret_key = 'shhhhitsasecret'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://rfacbcjizycwdl:58ec71e5deca626dea647631b65719669587a5b56f69c6b7072dceeeda368d29@ec2-50-17-203-51.compute-1.amazonaws.com:5432/dmiofp34d7sht';
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 
+# General Redirect
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def redir():
+    return redirect(url_for('index'))
+
+
+# Index
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+# Signup
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            # Username already exists. Do something meaninful
+            return redirect(url_for('signup'))
+        else:
+            user = User(username=username, password=sha256_crypt.hash(password))
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('index'))
+    else:
+        return render_template('signup.html')
 
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Signup')
