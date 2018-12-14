7e0ece6e455ebca38045f2b9ca7f403e62c4c070
mswilson4040 <mswilson4040@hotmail.com>
2018-12-13 19:51:45 -0800

    remove unused function

--- static/js/saved-reports.js
+++ static/js/saved-reports.js
@@ -3,22 +3,8 @@ class SavedReports {
         this.init();
     }
 
-    async init() {
+    init() {
 
-    }
-
-    getQueryString() {
-        let queryString = [];
-        if (this._year) {
-            queryString.push(`year=${this._year}`);
-        }
-        if (this._type) {
-            queryString.push(`type=${this._type}`);
-        }
-        if (this._district) {
-            queryString.push(`district=${this._district}`);
-        }
-        return queryString && queryString.length > 0 ? `?${queryString.join('&')}` : null;
     }
 }
 
